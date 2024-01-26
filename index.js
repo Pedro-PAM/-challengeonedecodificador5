3d7eacaa237c2a66dfc91c2e676255c8325d5772
Pedro-PAM <pedropamrj@gmail.com>
2024-01-26 20:35:18 -0300

    Completo

---
+++ index.js
@@ -0,0 +1,50 @@
+const textInput = document.getElementById('textInput');
+const criptografar = document.getElementById('criptografar');
+const descriptografar = document.getElementById('descriptografar');
+const mensagemCode = document.getElementById('mensagemCode');
+
+
+function criptoText(text){
+    // text = text.replace(/a/g, 'ai').replace(/e/g, 'enter').replace(/i/g, 'imes').replace(/o/g, 'ober').replace(/u/g, 'ufat');
+    text = text.replace(/imes/g, 'i').replace(/a/g, 'ai').replace(/e/g, 'enter').replace(/o/g, 'ober').replace(/u/g, 'ufat');    
+    icon = document.getElementById('nenhuma-mensagem');    
+    nomsg = document.getElementById('no-msg');
+    if(nomsg != null)
+        {
+            nomsg.hidden = true;
+        }
+    icon.innerHTML = `<h3 class=newMsg>${text}</h3>`;
+
+    
+}
+
+function decriptoText(text) {
+        icon = document.getElementById('nenhuma-mensagem');
+        icon.innerHTML = `<h3 class=newMsg>${text.replace(/ai/g, 'a').replace(/enter/g, 'e').replace(/imes/g, 'i').replace(/ober/g, 'o').replace(/ufat/g, 'u')}</h3>`
+        
+        
+}
+    
+    
+    
+    
+descriptografar.addEventListener('click', function()
+    {        
+        {            
+            const curText = textInput.value;
+            decriptoText(curText);
+        }        
+    }
+)
+        
+    
+
+
+
+
+criptografar.addEventListener('click',function(){    
+    criptoText(textInput.value);    
+})
+
+
+
